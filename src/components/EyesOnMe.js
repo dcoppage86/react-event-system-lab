8ba6a9ddefeb0a55102586d170defaa683ce4c4d
Dakota <dakotakcoppage@me.com>
2021-07-02 08:45:20 -0600

    commit tree 04bddcbe990c474b1bfbc91575a8072eeb34e24b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+    render() {
+
+        onFocus = () => console.log('Good!')
+
+        onblur = () => console.log('Hey! Eyes on me!')
+
+        return (
+            <div>
+                <button></button>
+            </div>
+        );
+    }
+}
+
+export default EyesOnMe;
